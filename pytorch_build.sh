4361832d2138300c7ca9589115a2a4aa04f3d0a2
AceBee007 <zykgcz@gmail.com>
2021-01-10 14:50:14 +0900

    Update pytorch_build.sh

--- pytorch_build.sh
+++ pytorch_build.sh
@@ -1,6 +1,6 @@
 echo 'Building docker'
 sudo docker build --network host -f pytorch_dockerfile -t acebee007/pytorch:latest .
 mkdir ../pytorch_workspace
-mkdir ../pytorch_workspace/tf
+mkdir ../pytorch_workspace/root
 mkdir ../pytorch_workspace/workspace
 mkdir ../pytorch_workspace/tblog
