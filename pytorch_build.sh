3e71d3c17ba37bad886a2ba7df17d1317054d382
AceBee007 <zykgcz@gmail.com>
2021-06-10 10:12:52 +0900

    move file into pytorch

--- pytorch_build.sh
+++
@@ -1,6 +0,0 @@
-echo 'Building docker'
-sudo docker build --network host -f pytorch_dockerfile -t acebee007/pytorch:latest .
-mkdir ../pytorch_workspace
-mkdir ../pytorch_workspace/root
-mkdir ../pytorch_workspace/root/workspace
-mkdir ../pytorch_workspace/tblog
